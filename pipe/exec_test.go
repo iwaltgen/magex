310c57bb9513bdf94eaad9418b493e2e4320ae5b
iwaltgen <iwaltgen@gmail.com>
2021-08-30 18:10:18 +0900

    feat: add exec test

---
+++ pipe/exec_test.go
@@ -0,0 +1,27 @@
+package pipe
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTouchRemove(t *testing.T) {
+	// dataset
+	dataset := []string{"test1", "test2", "test3"}
+
+	// table driven tests
+	for _, v := range dataset {
+		t.Run(v, func(t *testing.T) {
+			// when
+			cmds := []string{
+				"touch " + v,
+				"rm -f " + v,
+			}
+			err := ExecStdout(cmds...)
+
+			// then
+			assert.NoError(t, err)
+		})
+	}
+}
