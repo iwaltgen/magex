10b53f94ada178319674bfe7ba394d54cc7e90e4
iwaltgen <iwaltgen@gmail.com>
2021-09-02 17:35:41 +0900

    update: add unarchive failure test

--- archive/unarchive_test.go
+++ archive/unarchive_test.go
@@ -1,7 +1,6 @@
 package archive
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -12,7 +11,12 @@ import (
 )
 
 func TestUnarchive(t *testing.T) {
-	files, err := script.ListFiles("testdata").Slice()
+	files, err := script.ListFiles("testdata").EachLine(func(s string, b *strings.Builder) {
+		if strings.Contains(s, "invalid") {
+			return
+		}
+		b.WriteString(s + "\n")
+	}).Slice()
 	assert.NoError(t, err)
 
 	for _, v := range files {
@@ -21,7 +25,6 @@ func TestUnarchive(t *testing.T) {
 			// given
 			dest, err := os.MkdirTemp("", "magex-test-*")
 			assert.NoError(t, err)
-			fmt.Println(dest)
 			defer os.RemoveAll(dest)
 
 			// when
@@ -37,6 +40,57 @@ func TestUnarchive(t *testing.T) {
 			n, err := script.FindFiles(dest).CountLines()
 			assert.NoError(t, err)
 			assert.Equal(t, 5, n)
+		})
+	}
+}
+
+func TestUnarchiveReadOnly(t *testing.T) {
+	// given
+	files, err := script.ListFiles("testdata").EachLine(func(s string, b *strings.Builder) {
+		if strings.Contains(s, "invalid") {
+			return
+		}
+		b.WriteString(s + "\n")
+	}).Last(2).Slice()
+	assert.NoError(t, err)
+
+	for _, v := range files {
+		src := v
+		t.Run(filepath.Base(src), func(t *testing.T) {
+			// given
+			dest := "/etc/magex"
+
+			// when
+			err = Unarchive(src, dest)
+
+			// then
+			assert.Error(t, err)
+		})
+	}
+}
+
+func TestUnarchiveInvalidFormat(t *testing.T) {
+	files, err := script.ListFiles("testdata").EachLine(func(s string, b *strings.Builder) {
+		if !strings.Contains(s, "invalid") {
+			return
+		}
+		b.WriteString(s + "\n")
+	}).Slice()
+	assert.NoError(t, err)
+
+	for _, v := range files {
+		src := v
+		t.Run(filepath.Base(src), func(t *testing.T) {
+			// given
+			dest, err := os.MkdirTemp("", "magex-test-*")
+			assert.NoError(t, err)
+			defer os.RemoveAll(dest)
+
+			// when
+			err = Unarchive(src, dest)
+
+			// then
+			assert.Error(t, err)
 		})
 	}
 }
