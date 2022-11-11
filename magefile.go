2d135b41a274c1df34a2ed2b1224781778c10e05
iwaltgen <iwaltgen@gmail.com>
2022-11-11 23:00:17 +0900

    feat: remove deps go tool

--- magefile.go
+++ magefile.go
@@ -3,22 +3,16 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"time"
 
 	"github.com/fatih/color"
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
 
-	"github.com/iwaltgen/magex/dep"
 	"github.com/iwaltgen/magex/git"
 	"github.com/iwaltgen/magex/script"
 	"github.com/iwaltgen/magex/semver"
-	"github.com/iwaltgen/magex/spinner"
 )
-
-var goCmd = mg.GoCmd()
 
 // Run lint
 func Lint() error {
@@ -30,7 +24,7 @@ func Test() error {
 	mg.Deps(Lint)
 
 	return script.ExecStdout(
-		goCmd+" test ./... -json -coverprofile codecov.out -covermode atomic",
+		mg.GoCmd()+" test ./... -json -coverprofile codecov.out -covermode atomic",
 		"tparse -all",
 	)
 }
@@ -66,18 +60,4 @@ func Release(typ string) error {
 		color.Green(nv)
 	}
 	return err
-}
-
-// Run install dependency tool
-func Setup() error {
-	defer spinner.Start(100 * time.Millisecond)()
-
-	pkgs, err := dep.GlobImport("tool/deps.go")
-	if err != nil {
-		return fmt.Errorf("failed to load package import: %w", err)
-	}
-
-	args := []string{"install"}
-	args = append(args, pkgs...)
-	return sh.RunV(goCmd, args...)
 }
